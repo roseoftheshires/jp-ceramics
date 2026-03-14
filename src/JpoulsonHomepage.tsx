765d5dfdd13f2f874fd933c9d15ab015170df7fe
roseoftheshires <markot79@icloud.com>
2026-03-14 11:15:01 +0000

    Refactor JpoulsonHomepage component content

--- src/JpoulsonHomepage.tsx
+++ src/JpoulsonHomepage.tsx
@@ -1,15 +1,8 @@
-
-import React from "react"
-
 export default function JpoulsonHomepage() {
   return (
-    <div style={{
-      fontFamily: "sans-serif",
-      padding: "80px",
-      textAlign: "center"
-    }}>
+    <div style={{ padding: "80px", textAlign: "center", fontFamily: "sans-serif" }}>
       <h1>JP Ceramics</h1>
-      <p>React preview is working woohoo! 🎉</p>
+      <p>React preview working 🎉</p>
     </div>
   )
 }
