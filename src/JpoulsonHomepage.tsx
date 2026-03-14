f08237b4a4dcdcf8c8d663e9bf7589da433812b4
roseoftheshires <markot79@icloud.com>
2026-03-14 11:10:33 +0000

    Add JpoulsonHomepage component with basic layout

--- src/JpoulsonHomepage.tsx
+++ src/JpoulsonHomepage.tsx
@@ -1,1 +1,15 @@
 
+import React from "react"
+
+export default function JpoulsonHomepage() {
+  return (
+    <div style={{
+      fontFamily: "sans-serif",
+      padding: "80px",
+      textAlign: "center"
+    }}>
+      <h1>JP Ceramics</h1>
+      <p>React preview is working 🎉</p>
+    </div>
+  )
+}
